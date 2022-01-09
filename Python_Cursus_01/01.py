fc0c55157f6108c67e1672e5e323054a6cff720c
Theo <scanman91@gmail.com>
2022-01-09 15:14:33 +0100

    Merge remote-tracking branch 'origin/branch02' into branch02

---
+++ Python_Cursus_01/01.py
@@ -0,0 +1,6 @@
+print("Rekenmachine")
+First = input("Enter first number: ")
+Second = input("Enter second number: ")
+Sum=float(First)+float(Second)
+print("Sum " + str(Sum))
+
---
+++ Python_Cursus_01/02.py
@@ -0,0 +1,7 @@
+course="Python for Beginner"
+print(course.upper())
+print(course.replace('for','4'))
+print('Python' in course)
+print(course)
+
+
